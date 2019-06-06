b4846ef84e0daef22b12746346eefd878a6ff46d
Pavel Žák <pavel@berrycloud.cz>
2019-06-06 13:18:31 +0200

    Upgrade dependencies
    * Migrate from babel to @babel
    * Drop eslint, use standard.js

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -4,364 +4,363 @@ import qsm, {
   URL_TOGGLE,
   constructUrlParams,
   getUrlParams,
-  resolveUrlParams,
-} from '..';
-
+  resolveUrlParams
+} from '..'
 
 describe('qsm', () => {
   it('returns null when URL is a null', () => {
-    expect(qsm(null)).toBe(null);
-  });
+    expect(qsm(null)).toBe(null)
+  })
 
   it('returns null when URL is a number', () => {
-    expect(qsm(654654)).toBe(null);
-  });
+    expect(qsm(654654)).toBe(null)
+  })
 
   it('returns unchanged URL when given no actions and no query string', () => {
     expect(qsm('https://example.com/foo'))
-      .toBe('https://example.com/foo');
-  });
+      .toBe('https://example.com/foo')
+  })
 
   it('returns unchanged URL when given no actions', () => {
     expect(qsm('https://example.com/foo?bar=xx&baz=FoO'))
-      .toBe('https://example.com/foo?bar=xx&baz=FoO');
-  });
+      .toBe('https://example.com/foo?bar=xx&baz=FoO')
+  })
 
   it('returns unchanged URL when removed params are not found', () => {
     expect(qsm('https://example.com/foo?bar=xx', {
-      remove: ['foo'],
-    })).toBe('https://example.com/foo?bar=xx');
-  });
+      remove: ['foo']
+    })).toBe('https://example.com/foo?bar=xx')
+  })
 
   it('returns URL without query when params resolved as empty', () => {
     expect(qsm('https://example.com/foo?bar=xx', {
-      remove: ['bar'],
-    })).toBe('https://example.com/foo');
-  });
+      remove: ['bar']
+    })).toBe('https://example.com/foo')
+  })
 
   it('returns URL with newly set params', () => {
     expect(qsm('https://example.com/foo?bar=xx', {
       set: {
         bar: 'XXXXX',
-        page: 10,
-      },
-    })).toBe('https://example.com/foo?bar=XXXXX&page=10');
-  });
+        page: 10
+      }
+    })).toBe('https://example.com/foo?bar=XXXXX&page=10')
+  })
 
   it('works for example with adding page number', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world', {
       set: {
-        num: 20,
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=20');
-  });
+        num: 20
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=20')
+  })
 
   it('works for example with changing page number', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20', {
       set: {
-        num: 40,
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=40');
-  });
+        num: 40
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=40')
+  })
 
   it('works for example with removing page number', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20', {
-      remove: ['num'],
-    })).toBe('https://www.google.cz/search?q=hello+world');
-  });
+      remove: ['num']
+    })).toBe('https://www.google.cz/search?q=hello+world')
+  })
 
   it('works for example with removing page number and search', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20', {
-      remove: ['q', 'num'],
-    })).toBe('https://www.google.cz/search');
-  });
+      remove: ['q', 'num']
+    })).toBe('https://www.google.cz/search')
+  })
 
   it('works for example with toggling on filter', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20', {
       toggle: {
-        tbm: 'isch',
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=20&tbm=isch');
-  });
+        tbm: 'isch'
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=20&tbm=isch')
+  })
 
   it('works for example with toggling off filter', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20&tbm=isch', {
       toggle: {
-        tbm: 'isch',
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=20');
-  });
+        tbm: 'isch'
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=20')
+  })
 
   it('works for complex example with symbols', () => {
     expect(qsm('http://example.com/', {
       [URL_REMOVE]: ['test'],
       [URL_TOGGLE]: {
-        foo: 'bar',
+        foo: 'bar'
       },
       [URL_SET]: {
-        xxx: '123',
-      },
-    })).toBe('http://example.com/?xxx=123&foo=bar');
-  });
+        xxx: '123'
+      }
+    })).toBe('http://example.com/?xxx=123&foo=bar')
+  })
 
   it('works when removing param with symbols', () => {
     expect(qsm('http://example.com/?foo=bar&xxx=123&test=toBeRemoved', {
-      [URL_REMOVE]: ['test'],
-    })).toBe('http://example.com/?foo=bar&xxx=123');
-  });
+      [URL_REMOVE]: ['test']
+    })).toBe('http://example.com/?foo=bar&xxx=123')
+  })
 
   it('works when toggle-adding param with symbols', () => {
     expect(qsm('http://example.com/?foo=bar&xxx=123', {
       [URL_TOGGLE]: {
-        test: 'toBeAdded',
-      },
-    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded');
-  });
+        test: 'toBeAdded'
+      }
+    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded')
+  })
 
   it('works when toggle-changing-value param with symbols', () => {
     expect(qsm('http://example.com/?foo=bar&xxx=123&test=toBeChanged', {
       [URL_TOGGLE]: {
-        test: 'toBeAdded',
-      },
-    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded');
-  });
+        test: 'toBeAdded'
+      }
+    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded')
+  })
 
   it('works when toggle-removing param with symbols', () => {
     expect(qsm('http://example.com/?foo=bar&xxx=123&test=toBeRemoved', {
       [URL_TOGGLE]: {
-        test: 'toBeRemoved',
-      },
-    })).toBe('http://example.com/?foo=bar&xxx=123');
-  });
+        test: 'toBeRemoved'
+      }
+    })).toBe('http://example.com/?foo=bar&xxx=123')
+  })
 
   it('works when setting param with symbols', () => {
     expect(qsm('http://example.com/?foo=bar&xxx=123', {
       [URL_TOGGLE]: {
-        test: 'toBeAdded',
-      },
-    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded');
-  });
+        test: 'toBeAdded'
+      }
+    })).toBe('http://example.com/?foo=bar&xxx=123&test=toBeAdded')
+  })
 
   it('works with urldecoded address', () => {
     expect(qsm('https://example.org?f%5BnumPages%5D%5Bv%5D%5Bmin%5D=1', {
       [URL_SET]: {
-        'f[numPages][v][min]': 2,
-      },
-    })).toBe('https://example.org?f%5BnumPages%5D%5Bv%5D%5Bmin%5D=2');
-  });
+        'f[numPages][v][min]': 2
+      }
+    })).toBe('https://example.org?f%5BnumPages%5D%5Bv%5D%5Bmin%5D=2')
+  })
 
   it('works with example for array paramters', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=20', {
       [URL_SET]: {
-        num: [20, 40, 60],
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=20&num=40&num=60');
-  });
+        num: [20, 40, 60]
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=20&num=40&num=60')
+  })
 
   it('works with ordered parameter arrays', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=40', {
       [URL_SET]: {
-        num: [20, 40, 60],
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=20&num=40&num=60');
-  });
+        num: [20, 40, 60]
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=20&num=40&num=60')
+  })
 
   it('sets params from array when URL contains more values than input', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=x3&num=x4', {
       [URL_SET]: {
-        num: [12],
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=12');
-  });
+        num: [12]
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=12')
+  })
 
   it('sets params from array when URL contains less values than input', () => {
     expect(qsm('https://www.google.cz/search?q=hello+world&num=x3&num=x4', {
       [URL_SET]: {
-        num: [12, 13, 14, 15],
-      },
-    })).toBe('https://www.google.cz/search?q=hello+world&num=12&num=13&num=14&num=15');
-  });
+        num: [12, 13, 14, 15]
+      }
+    })).toBe('https://www.google.cz/search?q=hello+world&num=12&num=13&num=14&num=15')
+  })
 
   it('removes all ocurrences of params URL', () => {
     expect(qsm('https://www.google.cz/search?num=12&q=hello+world&num=x3&num=x4', {
-      [URL_REMOVE]: ['num'],
-    })).toBe('https://www.google.cz/search?q=hello+world');
-  });
-});
+      [URL_REMOVE]: ['num']
+    })).toBe('https://www.google.cz/search?q=hello+world')
+  })
+})
 
 describe('lib', () => {
   it('provides set constant', () => {
-    expect(URL_SET).toBe('set');
-  });
+    expect(URL_SET).toBe('set')
+  })
 
   it('provides remove constant', () => {
-    expect(URL_REMOVE).toBe('remove');
-  });
+    expect(URL_REMOVE).toBe('remove')
+  })
 
   it('provides toggle constant', () => {
-    expect(URL_TOGGLE).toBe('toggle');
-  });
-});
+    expect(URL_TOGGLE).toBe('toggle')
+  })
+})
 
 describe('getUrlParams', () => {
   it('returns empty array for absolute URI without query string', () => {
-    expect(getUrlParams('https://example.com/test')).toEqual([]);
-  });
+    expect(getUrlParams('https://example.com/test')).toEqual([])
+  })
 
   it('returns empty array for absolute URL without path and query string', () => {
-    expect(getUrlParams('https://example.com')).toEqual([]);
-  });
+    expect(getUrlParams('https://example.com')).toEqual([])
+  })
 
   it('returns empty array for absolute URL with root path and query string', () => {
-    expect(getUrlParams('https://example.com/')).toEqual([]);
-  });
+    expect(getUrlParams('https://example.com/')).toEqual([])
+  })
 
   it('returns empty array for relative URL without query string', () => {
-    expect(getUrlParams('/local')).toEqual([]);
-  });
+    expect(getUrlParams('/local')).toEqual([])
+  })
 
   it('returns empty array for URL with empty query string', () => {
-    expect(getUrlParams('/local?')).toEqual([]);
-  });
+    expect(getUrlParams('/local?')).toEqual([])
+  })
 
   it('returns URL params as array', () => {
     expect(getUrlParams('/local?foo=bar&bar=123')).toEqual([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ]);
-  });
+      { key: 'bar', value: '123' }
+    ])
+  })
 
   it('returns URL params as array with repeated params', () => {
     expect(getUrlParams('/local?foo=bar&foo=123&foo=xxx')).toEqual([
       { key: 'foo', value: 'bar' },
       { key: 'foo', value: '123' },
-      { key: 'foo', value: 'xxx' },
-    ]);
-  });
+      { key: 'foo', value: 'xxx' }
+    ])
+  })
 
   it('returns params of urldecoded address', () => {
     expect(getUrlParams('https://example.org?f%5BnumPages%5D%5Bv%5D%5Bmin%5D=1')).toEqual([
       {
         key: 'f[numPages][v][min]',
-        value: '1',
-      },
-    ]);
-  });
-});
+        value: '1'
+      }
+    ])
+  })
+})
 
 describe('resolveUrlParams', () => {
   it('returns prev params untouched when no action was passed', () => {
     const urlParams = [
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ];
-    const paramActions = [];
+      { key: 'bar', value: '123' }
+    ]
+    const paramActions = []
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ]);
-  });
+      { key: 'bar', value: '123' }
+    ])
+  })
 
   it('returns newly set params', () => {
-    const urlParams = [];
+    const urlParams = []
     const paramActions = {
       set: {
         foo: 'bar',
-        bar: '123',
-      },
-    };
+        bar: '123'
+      }
+    }
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ]);
-  });
+      { key: 'bar', value: '123' }
+    ])
+  })
 
   it('returns params where set replaces previous value', () => {
     const urlParams = [
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ];
+      { key: 'bar', value: '123' }
+    ]
     const paramActions = {
       set: {
-        bar: '456',
-      },
-    };
+        bar: '456'
+      }
+    }
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '456' },
-    ]);
-  });
+      { key: 'bar', value: '456' }
+    ])
+  })
 
   it('returns params where set replaces previous array value', () => {
     const urlParams = [
       { key: 'foo', value: 'bar' },
-      { key: 'foo', value: '123' },
-    ];
+      { key: 'foo', value: '123' }
+    ]
     const paramActions = {
       set: {
-        foo: ['xxx', '456'],
-      },
-    };
+        foo: ['xxx', '456']
+      }
+    }
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       { key: 'foo', value: 'xxx' },
-      { key: 'foo', value: '456' },
-    ]);
-  });
+      { key: 'foo', value: '456' }
+    ])
+  })
 
   it('returns params where toggle sets param value', () => {
     const urlParams = [
-      { key: 'foo', value: 'bar' },
-    ];
+      { key: 'foo', value: 'bar' }
+    ]
     const paramActions = {
       toggle: {
-        bar: '456',
-      },
-    };
+        bar: '456'
+      }
+    }
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '456' },
-    ]);
-  });
+      { key: 'bar', value: '456' }
+    ])
+  })
 
   it('returns params where toggle removes param value', () => {
     const urlParams = [
-      { key: 'foo', value: 'bar' },
-    ];
+      { key: 'foo', value: 'bar' }
+    ]
     const paramActions = {
       toggle: {
-        foo: 'bar',
-      },
-    };
-    expect(resolveUrlParams(urlParams, paramActions)).toEqual([]);
-  });
+        foo: 'bar'
+      }
+    }
+    expect(resolveUrlParams(urlParams, paramActions)).toEqual([])
+  })
 
   it('returns params where toggle sets param value when its found, but different value', () => {
     const urlParams = [
-      { key: 'foo', value: 'bar' },
-    ];
+      { key: 'foo', value: 'bar' }
+    ]
     const paramActions = {
       toggle: {
-        foo: 'barz',
-      },
-    };
+        foo: 'barz'
+      }
+    }
     expect(resolveUrlParams(urlParams, paramActions)).toEqual([
       {
         key: 'foo',
-        value: 'barz',
-      },
-    ]);
-  });
-});
+        value: 'barz'
+      }
+    ])
+  })
+})
 
 describe('constructUrlParams', () => {
   it('returns empty string for empty params', () => {
-    expect(constructUrlParams([])).toBe('');
-  });
+    expect(constructUrlParams([])).toBe('')
+  })
 
   it('returns ampersand joined params with values', () => {
     expect(constructUrlParams([
       { key: 'foo', value: 'bar' },
-      { key: 'bar', value: '123' },
-    ])).toBe('foo=bar&bar=123');
-  });
-});
+      { key: 'bar', value: '123' }
+    ])).toBe('foo=bar&bar=123')
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,93 +1,93 @@
-export const URL_REMOVE = 'remove';
-export const URL_SET = 'set';
-export const URL_TOGGLE = 'toggle';
+export const URL_REMOVE = 'remove'
+export const URL_SET = 'set'
+export const URL_TOGGLE = 'toggle'
 
-const filterByParamName = paramName => param => param.key === paramName;
+const filterByParamName = paramName => param => param.key === paramName
 
-const findParamIndex = (params, paramName) => params.findIndex(filterByParamName(paramName));
+const findParamIndex = (params, paramName) => params.findIndex(filterByParamName(paramName))
 
 const removeParam = (params, paramName) => {
-  let paramIndex;
+  let paramIndex
   do {
-    paramIndex = params.findIndex(filterByParamName(paramName));
+    paramIndex = params.findIndex(filterByParamName(paramName))
     if (paramIndex !== -1) {
-      params.splice(paramIndex, 1);
+      params.splice(paramIndex, 1)
     }
-  } while (paramIndex !== -1);
-};
+  } while (paramIndex !== -1)
+}
 
-const findQueryStart = url => url.indexOf('?');
+const findQueryStart = url => url.indexOf('?')
 
 const removeParams = (urlParams, params) =>
-  params.forEach(paramName => removeParam(urlParams, paramName));
+  params.forEach(paramName => removeParam(urlParams, paramName))
 
 const mapInputKeyToParam = (params, key) => ({
   key,
-  value: params[key],
-});
+  value: params[key]
+})
 
 const mapInputToParams = params => Object.keys(params).reduce((aggr, key) => {
   if (params[key] instanceof Array) {
-    return aggr.concat(params[key].map(value => ({ key, value })));
+    return aggr.concat(params[key].map(value => ({ key, value })))
   }
 
-  return [...aggr, mapInputKeyToParam(params, key)];
-}, []);
+  return [...aggr, mapInputKeyToParam(params, key)]
+}, [])
 
 const toggleParams = (urlParams, urlParamsNext, params) => {
   Object.keys(params).forEach((key) => {
-    const paramIndex = findParamIndex(urlParams, key);
-    const value = params[key];
+    const paramIndex = findParamIndex(urlParams, key)
+    const value = params[key]
     if (paramIndex === -1) {
-      urlParamsNext.push({ key, value });
+      urlParamsNext.push({ key, value })
     } else {
-      const param = urlParams[paramIndex];
+      const param = urlParams[paramIndex]
       if (param.value === value) {
-        removeParam(urlParams, key);
+        removeParam(urlParams, key)
       } else {
-        urlParamsNext.push({ key, value });
+        urlParamsNext.push({ key, value })
       }
     }
-  });
-};
+  })
+}
 
 export const getUrlParams = (url) => {
-  const decodedUrl = decodeURI(url);
-  const queryStart = findQueryStart(decodedUrl);
-  const urlParamPairs = queryStart === -1 ? [] : decodedUrl.substr(queryStart + 1).split('&');
+  const decodedUrl = decodeURI(url)
+  const queryStart = findQueryStart(decodedUrl)
+  const urlParamPairs = queryStart === -1 ? [] : decodedUrl.substr(queryStart + 1).split('&')
   return urlParamPairs.filter(param => param).map((paramPair) => {
-    const [key, value] = paramPair.split('=');
-    return { key, value };
-  });
-};
+    const [key, value] = paramPair.split('=')
+    return { key, value }
+  })
+}
 
 export const resolveUrlParams = (prevParams, paramActions) => {
-  const urlParams = prevParams.slice();
-  const urlParamsNext = paramActions[URL_SET] ? mapInputToParams(paramActions[URL_SET]) : [];
+  const urlParams = prevParams.slice()
+  const urlParamsNext = paramActions[URL_SET] ? mapInputToParams(paramActions[URL_SET]) : []
 
   if (paramActions[URL_TOGGLE]) {
-    toggleParams(urlParams, urlParamsNext, paramActions[URL_TOGGLE]);
+    toggleParams(urlParams, urlParamsNext, paramActions[URL_TOGGLE])
   }
 
   if (paramActions[URL_REMOVE]) {
-    removeParams(urlParams, paramActions[URL_REMOVE]);
+    removeParams(urlParams, paramActions[URL_REMOVE])
   }
 
   return [
     ...urlParams.filter(param => findParamIndex(urlParamsNext, param.key) === -1),
-    ...urlParamsNext,
-  ];
-};
+    ...urlParamsNext
+  ]
+}
 
 export const constructUrlParams = params =>
-  params.map(param => `${encodeURI(param.key)}=${encodeURI(param.value)}`).join('&');
+  params.map(param => `${encodeURI(param.key)}=${encodeURI(param.value)}`).join('&')
 
 export default (url, paramActions = {}) => {
   if (!url || typeof url !== 'string') {
-    return null;
+    return null
   }
-  const params = constructUrlParams(resolveUrlParams(getUrlParams(url), paramActions));
-  const queryStart = findQueryStart(url);
-  const strippedUrl = queryStart === -1 ? url : url.substr(0, queryStart);
-  return params.length === 0 ? strippedUrl : `${strippedUrl}?${params}`;
-};
+  const params = constructUrlParams(resolveUrlParams(getUrlParams(url), paramActions))
+  const queryStart = findQueryStart(url)
+  const strippedUrl = queryStart === -1 ? url : url.substr(0, queryStart)
+  return params.length === 0 ? strippedUrl : `${strippedUrl}?${params}`
+}
